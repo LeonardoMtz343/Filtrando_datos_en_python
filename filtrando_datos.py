49933c678a029dea6d16e6bd6fd0e5538a2a813d
Leonardo Martinez <leoztrend@gmail.com>
2021-06-10 21:33:26 -0500

    Reto terminado

--- filtrando_datos.py
+++ filtrando_datos.py
@@ -73,23 +73,6 @@ DATA = [
 
 def run():
 
-    # '''Vamos a obtener unicamente el nombre de los programadores de python'''
-    # all_pyhton_devs = [worker ['name'] for worker in DATA if worker['language'] == 'python']
-
-    # '''Vamos a obtener unicamente los trabajadores de Platzi'''
-    # all_workers_platzi = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi']
-
-    # '''Vamos a obtener unicamente los adultos >18 años con filter'''
-    # all_adults = list(filter(lambda worker: worker['age']>=18, DATA))
-
-    # '''Anadimos un MAP para poder mostrar solo el nombre'''
-    # all_adults=list(map(lambda worker: worker['name'], all_adults))
-
-    # '''Aqui se agrega una nueva llave al diccionario all_adults que nos dice si el worker es mayor de 70 años'''
-    # old_people =list(map(lambda worker: worker | {'old': worker['age']>70}, DATA))
-
-    # RETO 
-
     ''' Vamos a obtener unicamente el nombre de los programadores de python usando Filter y map'''
 
     all_python_devs = list(filter(lambda worker: worker['language'] == 'python', DATA))
